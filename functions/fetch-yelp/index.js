4112c9fba8327274faacddef85e571d67e0faa64
Tyler Watson <tylerww91@gmail.com>
2023-02-06 20:58:58 -0800

    builds out handler in index.js, adds onChange for both inputs that set either Zip or Search state

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -2,6 +2,27 @@ const fetch = require('node-fetch');
 require('dotenv').config({ path: `.env.development.local` });
 
 const handler = async (event) => {
+  const zip = event.queryStringParameters.zip;
+  const search = event.queryStringParameters.search;
+
+  try {
+    const resp = await fetch(
+      `https://api.yelp.com/v3/businesses/search?location=${zip}&term=${search}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+        },
+      }
+    );
+    const data = await resp.json();
+    const json = JSON.stringify(data.businesses);
+    return {
+      statusCode: 200,
+      body: json,
+    };
+  } catch (e) {
+    console.error(e.message);
+  }
   // add code here to fetch data from yelp API
   // be sure to include the parameters from event.queryStringParameters
 };
--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { fetchBusinesses } from './services/yelp';
 function App() {
   const [businesses, setBusinesses] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [zip, setZip] = useState('97003');
+  const [search, setSearch] = useState('');
 
   // TODO -- add state for zip / search and add event listeners to the inputs
 
@@ -20,19 +22,43 @@ function App() {
 
   // TODO -- add event for button click to handle calling fetchBusinesses with zip / search
 
+  // const handleSearch = async () => {
+  //   // const resp = await fetchBusinesses(zip, search);
+  //   // const data = await resp.json();
+  //   // console.log(data);
+  //   // setBusinesses(data);
+  // };
+
   return (
     <div className="App">
       <h1>Alchemy Restaurant Finder</h1>
       <div className="query-form">
         <div className="form-control">
           <label>Zip:</label>
-          <input type="text" placeholder="zip" />
+          <input
+            type="text"
+            placeholder="zip"
+            value={zip}
+            onChange={(e) => setZip(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label>Query:</label>
-          <input type="text" placeholder="Search..." />
+          <input
+            type="text"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
-        <button>Search</button>
+        <button
+          onClick={async () => {
+            const resp = await fetchBusinesses(zip, search);
+            setBusinesses(resp);
+          }}
+        >
+          Search
+        </button>
       </div>
       {loading && <div className="loader"></div>}
       {!loading && businesses.map((b) => <RestaurantListItem key={b.id} {...b} />)}
